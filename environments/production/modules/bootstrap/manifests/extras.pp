bcd5ce0a397d217707cebf583db6d53334232618
Kalman Olah <hello@kalmanolah.net>
2015-06-06 14:01:23 +0200

    TMSU added.
    Signed-off-by: Kalman Olah <hello@kalmanolah.net>

--- environments/production/modules/bootstrap/manifests/extras.pp
+++ environments/production/modules/bootstrap/manifests/extras.pp
@@ -1,3 +1,4 @@
+# Class for bootstrapping extra stuff
 class bootstrap::extras (
   $files    = {},
   $augeas   = {},
@@ -5,6 +6,7 @@ class bootstrap::extras (
   $packages = {},
   $mounts   = {},
   $execs    = {},
+  $archives = {}
 ) {
   $file_defaults = {
     ensure => 'present',
@@ -29,10 +31,15 @@ class bootstrap::extras (
     path => '/usr/bin:/usr/local/bin:/bin:/usr/local/sbin',
   }
 
+  $archive_defaults = {
+    ensure => 'present',
+  }
+
   create_resources(file, $files, $file_defaults)
   create_resources(augeas, $augeas, $augeas_defaults)
   create_resources(vcsrepo, $vcsrepos, $vcsrepo_defaults)
   create_resources(package, $packages, $package_defaults)
   create_resources(mount, $mounts, $mount_defaults)
   create_resources(exec, $execs, $exec_defaults)
+  create_resources(archive, $archives, $archive_defaults)
 }
