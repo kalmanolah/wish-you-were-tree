0707f0a4e2df8952d8f5da60a4cfcfb8bdd287de
Kalman Olah <hello@kalmanolah.net>
2017-01-07 22:16:03 +0100

    Qbittorrent firewall rules.
    Signed-off-by: Kalman Olah <hello@kalmanolah.net>

--- code/environments/production/modules/profile_firewall/manifests/pre.pp
+++ code/environments/production/modules/profile_firewall/manifests/pre.pp
@@ -4,7 +4,7 @@ class profile_firewall::pre {
     }
 
     firewallchain {
-        'INPUT:filter:IPv4':
+        ['INPUT:filter:IPv4', 'INPUT:filter:IPv6']:
             ensure => 'present',
             purge  => true,
             policy => 'accept',
@@ -17,25 +17,51 @@ class profile_firewall::pre {
             ];
     }
 
-    # Default firewall rules
-    firewall { '000 accept all icmp':
+    # Default firewall rules (IPv4)
+    firewall { '000 accept all icmp (IPv4)':
         proto  => 'icmp',
         action => 'accept',
     }->
-    firewall { '001 accept all to lo interface':
+    firewall { '001 accept all to lo interface (IPv4)':
         proto   => 'all',
         iniface => 'lo',
         action  => 'accept',
     }->
-    firewall { '002 reject local traffic not on loopback interface':
+    firewall { '002 reject local traffic not on loopback interface (IPv4)':
         iniface     => '! lo',
         proto       => 'all',
         destination => '127.0.0.1/8',
         action      => 'reject',
     }->
-    firewall { '003 accept related established rules':
+    firewall { '003 accept related established rules (IPv4)':
         proto  => 'all',
         state  => ['RELATED', 'ESTABLISHED'],
         action => 'accept',
+    }
+
+    # Default firewall rules (IPv6)
+    firewall { '000 accept all icmp (IPv6)':
+        proto    => 'icmp',
+        action   => 'accept',
+        provider => 'ip6tables',
+    }->
+    firewall { '001 accept all to lo interface (IPv6)':
+        proto    => 'all',
+        iniface  => 'lo',
+        action   => 'accept',
+        provider => 'ip6tables',
+    }->
+    firewall { '002 reject local traffic not on loopback interface (IPv6)':
+        iniface     => '! lo',
+        proto       => 'all',
+        destination => '::1/128',
+        action      => 'reject',
+        provider    => 'ip6tables',
+    }->
+    firewall { '003 accept related established rules (IPv6)':
+        proto    => 'all',
+        state    => ['RELATED', 'ESTABLISHED'],
+        action   => 'accept',
+        provider => 'ip6tables',
     }
 }
